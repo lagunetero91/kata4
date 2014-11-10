e984bca942a30af6451476a08b9fea0d487656aa
usuario <usuario@10.22.143.36>
2014-11-10 13:16:47 +0000

    Clase DataBasePersonLoader implementada

--- src/kata4/DataBasePersonLoader.java
+++ src/kata4/DataBasePersonLoader.java
@@ -1,6 +1,9 @@
 package kata4;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DataBasePersonLoader implements PersonLoader {
     private final Connection connection;
@@ -11,7 +14,17 @@ public class DataBasePersonLoader implements PersonLoader {
 
     @Override
     public Person[] load() {
+        try{
+        return processQuery(connection.createStatement().executeQuery("SELECT * FROM person"));
+       } catch (SQLException ex) {
+           return new Person[0];
+       }
         
+    }
+
+    private Person[] processQuery(ResultSet executeQuery) {
+        ArrayList<Person> personList = new ArrayList<>();
+        return personList.toArray(new Person[personList.size()]);
     }
     
 }
