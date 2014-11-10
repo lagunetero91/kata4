405ff3f2453edd6d44253ca5c0d7347f8dee269d
usuario <usuario@10.22.143.36>
2014-11-10 13:38:29 +0000

    Main - comienzo de implementacion

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,8 +1,16 @@
 package kata4;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Kata4 {
 
     public static void main(String[] args){
+        Connection connection = createConnection("people.db");
+    }
+
+    private static Connection createConnection(String peopledb) {
+        DriverManager.registerDriver(new org.sqlite.JDBC());
     }
     
 }
