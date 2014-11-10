5518f81d080a03b449794ca2af610371e0f38c36
usuario <usuario@10.22.143.36>
2014-11-10 13:54:05 +0000

    Corregidos fallos de compilacion

--- src/kata4/Person.java
+++ src/kata4/Person.java
@@ -22,6 +22,10 @@ public class Person {
         this.web = web;
     }
 
+    Person() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     public String getFirstName() {
         return firstName;
     }
